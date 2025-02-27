138921092f520d55b359ea5c38af9edde37d8230
Alfian Kafilah Baits <alfiank@stellargeloratech.com>
2025-02-27 15:21:30 +0700

    feat(change-flow): change flow of register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -36,6 +36,12 @@
             </label>
         </div>
 
+        <!-- Register -->
+        <div class="block mt-2">
+            <a class="text-sm text-gray-600"> Not Registered? </a>
+            <a class="text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"  href="/register">Click here!</a>
+        </div>
+
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
